966fbc8d4d4c4f8dbbf1005393a8a5feeabc2548
SEH00N <06psh0501@gmail.com>
2022-10-18 10:36:49 +0900

    dargon movement

---
+++ DragonFlightClient/Assets/01. Scripts/Dragon/DragonMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class DragonMovement : MonoBehaviour
+{
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float runSpeed = 10f;
+    [SerializeField] float animBelnd = 0f;
+    private float speed;
+
+    private Rigidbody rb = null;
+    private Animator anim = null;
+
+    private bool onGround = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        speed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if(Input.GetKey(KeyCode.LeftShift) && speed < runSpeed)
+            speed += Time.deltaTime * 2f;
+        else if(!Input.GetKey(KeyCode.LeftShift) && speed > walkSpeed)
+            speed -= Time.deltaTime * 2f;
+
+        transform.Translate(new Vector3(x, 0, z) * Time.deltaTime * speed);
+
+        animBelnd = Mathf.Lerp(0, speed, Mathf.Abs(z));
+        anim.SetFloat("Move", animBelnd);
+    }
+}
